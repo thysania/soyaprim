8f657c0f9639dc03d48d63e733aa38450bd41616
thysania <133405532+thysania@users.noreply.github.com>
2025-03-23 18:33:14 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# Sidebar for app selection
+st.sidebar.title("Navigation")
+app_choice = st.sidebar.radio(
+    "Choisissez une application :",
+    ("bq_ref", "bq", "achats")
+)
+
+# Import and run the selected app
+if app_choice == "bq_ref":
+    from bq_ref import app2 as bq_ref_app
+    bq_ref_app()
+elif app_choice == "bq":
+    from bq import app as bq_app
+    bq_app()
+elif app_choice == "achats":
+    from achats import app as achats_app
+    achats_app()
