067992edde1ecc9b42c061ae65ededc03c9a11a4
thysania <133405532+thysania@users.noreply.github.com>
2025-03-30 14:01:18 +0000

    Update bq.py

--- bq.py
+++ bq.py
@@ -92,7 +92,7 @@ def app():
                     # Rule 3: RAW_TIER == "CNSS" or NAT == "COTIS"
                     (raw_df["RAW_TIER"].astype(str).str.upper() == "CNSS") | (raw_df["RAW_REF"] == "COTIS"),
                     # Rule 4: RAW_REF == FRAIS
-                    (raw_df["RAW_REF"] == "FRAIS"),
+                    (raw_df["RAW_REF"] == "FRAIS") | raw_df["RAW_LIB"].astype(str).str.upper().str.startswith("FRAIS", na=False),
                     # Rule 5: RAW_LIB starts with "diff" or contains "change"
                     raw_df["RAW_LIB"].astype(str).str.upper().str.contains("DIFF", na=False) | 
                     raw_df["RAW_LIB"].astype(str).str.upper().str.contains("CHANGE", na=False),
